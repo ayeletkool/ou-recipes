ddd0a270785916ff7a111941d09e9d170500bc27
ayeletkool <ayeletmfriedman@gmail.com>
2015-11-22 12:19:25 +0200

    deletes vue files and modifies scripts to cdn links

--- recipes.js
+++ recipes.js
@@ -1,7 +1,4 @@
-
 new Vue({    
-
-
     el: '#recipes',    
     ready:function(){
     this.fetchRecipes();
@@ -10,19 +7,16 @@ new Vue({
         recipes : []
 //        for testing purposes
 //        recipes : [{"post_title":"Meatballs", "recipe_type":"Meat"},{"post_title":"Cheesecake", "recipe_type":"Dairy"},{"post_title":"Stir-Fry", "recipe_type":"Pareve"},{"post_title":"Onion soup", "recipe_type":"Dairy and Pareve"},{"post_title":"Vegetable soup", "recipe_type":"Meat and Pareve"}]
-    }
+    },
         methods:{
         fetchRecipes:function(){
-            this.$https.get('https://es.content-index.oustatic.com:8443/rsstage_oukosher_org/_search?q=post_type:recipes').success(function(data){
-                this.recipes = data;
-            }).error(function(data, status, request){
+            this.$http.get('https://orthodox:union@es.content-index.oustatic.com:8443/rsstage_oukosher_org/_search?q=post_type:recipes',
+                function(data){
+                this.$set('recipes', data.hits.hits)
+            }).error(function(status){
                 //handle error
-                console.log(data);
                 console.log(status);
-                console.log(request);
-                console.log(error);
             }); 
         }
     }
-
 });
