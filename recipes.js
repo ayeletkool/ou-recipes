a476b8eb5e56aca7046f7388082f9f5574281a21
ayeletkool <ayeletmfriedman@gmail.com>
2015-11-23 20:46:51 +0200

    100 hits

--- recipes.js
+++ recipes.js
@@ -8,7 +8,7 @@ new Vue({
     },
         methods:{
         fetchRecipes:function(){
-            this.$http.get('https://orthodox:union@es.content-index.oustatic.com:8443/rsstage_oukosher_org/_search', {q:"post_type:recipes",size:"10"},
+            this.$http.get('https://orthodox:union@es.content-index.oustatic.com:8443/rsstage_oukosher_org/_search', {q:"post_type:recipes",size:"100"},
                 function(data){
                 this.$set('recipes', data.hits.hits)
             }).error(function(status){
